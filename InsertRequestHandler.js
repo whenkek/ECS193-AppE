96f99cb0c2842a37f52f6da5b232172a0f438ff8
Mrolas <Nicholas.Michael.Ng@gmail.com>
2018-02-19 21:34:40 -0800

    Week 6?

--- InsertRequestHandler.js
+++ InsertRequestHandler.js
@@ -2,58 +2,143 @@ var process = require('process');
 var express = require('express');
 var Knex = require('knex');
 
-function testInsert (knex, req, res)
+function insertPatient (knex, req, res)
 {
-	body = req.body;
-	resmsg = "";
-	errmsg = "";
-	console.log(body)
-	success = true
-	for( var key in  body ) //foreachtable
-	{
-		table_name = key; 
-		// format table_name:{Object1, Object2}; Object1 and Object2 are the string representation of json objects and must be parsed
-		if(Array.isArray(body[key])){
-			values = body[key].map( x => JSON.parse(x));
-			msg = "INSERT INTO " +table_name +` VALUES ${body[key].join(", ")}`; //purely debug
-		}
-		else{
-			values = JSON.parse(body[key])
-			msg = "INSERT INTO " +table_name +` VALUES ${body[key]}`; //purely debug
-		}
-		console.log(values)
-		resmsg += msg;
-		knex.schema.hasTable(table_name).then( (exists) =>{
-			if(exists)
+	var data = req.body;
+
+	knex('doctors')
+		.where('name', data.doctor)
+		.catch((err) => { console.log(err); })
+		.then(function(rows) {
+			if (rows.length >= 1)
 			{
-				knex.transaction((trx) => {
-					knex(table_name).insert(values).then ( (result) => {
-						console.log("hi");
-						//res.json({ success: true, message: 'ok' });} )
+				knex('patients')
+					.insert(data)
+					.catch((err) => { console.log(err); })
+					.then(function() {
+						knex('patients')
+							.orderBy('id', 'desc')
+							.limit(1)
+							.then(function(rows) {
+								var row = rows[0];
+								var id = row.id;
+								var tableName = 'patient_' + id;
+								knex.schema.
+									createTable(tableName, function(table) {
+										table.dateTime('timestamp').defaultTo(knex.fn.now()).primary();
+										for (var i = 0; i < 4; i++)
+											table.float('ch' + i).notNullable();
+									})
+									.then(function() {
+										res.status(200)
+											.set('Content-Type', 'text/plain')
+											.send('Received: ' + JSON.stringify(data))
+											.end();
+									});
+							});
 					})
-				.then(function(inserts) 
-				{
-					console.log(inserts.length + ' new books saved.');
-				})
-				})
-				.catch((err) => {
-					errmsg += err + msg + '\n';
-					success = false
-				});
+			}
+			else
+			{
+				res.status(200)
+					.set('Content-Type', 'text/plain')
+					.send('No doctor of name exists.')
+					.end();
 			}
 		});
-	}
-	console.log(errmsg)
-	if(success)
-		res.status(200)
-			.set('Content-Type', 'text/plain')
-			.send(resmsg)
-			.end();
-	else
-		res.status(400)
-		.set('Content-Type', 'text/plain')
-		.send("The following commands had errors: " + errmsg)
-		.end();
 }
 
-module.exports.testInsert = testInsert;
+function insertDoctor (knex, req, res)
+{
+	var data = req.body;
+
+	knex('doctors')
+		.where('name', data.name)
+		.catch((err) => { console.log(err); })
+		.then(function(rows) {
+			if (rows.length >= 1)
+			{
+				res.status(200)
+					.set('Content-Type', 'text/plain')
+					.send('Doctor of same name already exists.')
+					.end();
+			}
+			else
+			{
+				knex('doctors')
+					.insert(data)
+					.catch((err) => { console.log(err); })
+					.then(function() {
+						res.status(200)
+							.set('Content-Type', 'text/plain')
+							.send('Received: ' + JSON.stringify(data))
+							.end();
+					});
+			}
+		});
+}
+
+function insertReading (knex, req, res)
+{
+	var body = req.body;
+	var resmsg = '';
+	var errmsg = '';
+	console.log(body);
+	var success = true;
+	var msg = '';
+	var table_name = '';
+	
+	console.log('body: ' + req.body);
+	for (var key in body)
+	{
+		console.log('key: ' + key);
+		table_name = key; 
+		if(Array.isArray(body[key]))
+			values = body[key].map(x => JSON.parse(x));
+		else
+			try 
+			{
+				values = JSON.parse(body[key]);
+			}
+			catch(e) 
+			{
+				values = body[key];
+			}
+			break;
+	}
+
+	temp = knex(table_name).insert(values);
+	knex.schema.hasTable(table_name).then(function (exists) {
+		if(exists)
+		{
+			knex
+				.transaction(function (trx) {
+					temp.then (function (result) {
+						res.status(200)
+							.set('Content-Type', 'text/plain')
+							.send("All insertions were success.")
+							.end();
+					})
+				})
+				.catch(function (err) {
+					errmsg += err;				
+					res.status(400)
+						.set('Content-Type', 'text/plain')
+						.send("The following commands had errors: " + errmsg)
+						.end();
+				});
+		}
+		else
+		{
+			errmsg += "Table: " + table_name + " does not exist.\n";
+			res.status(400)
+				.set('Content-Type', 'text/plain')
+				.send("The following commands had errors: " + errmsg)
+				.end();
+		}
+	});
+}
+
+module.exports.insertPatient = insertPatient;
+module.exports.insertDoctor = insertDoctor;
+module.exports.insertReading = insertReading;
--- server.js
+++ server.js
@@ -10,36 +10,33 @@ var multer = require('multer');
 var upload = multer();
 
 
-var bodyParser = require('body-parser')
-//	app.use(bodyParser.json()); // support json encoded bodies
-	//app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-	//app.use(bodyParser.raw({ extended: true })); // support encoded bodies
-	//app.use(bodyParser.text({ extended: true })); // support encoded bodies
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+app.use(jsonParser);
 
 app.enable('trust proxy');
 
 var knex = Connect()
 
+process.env.NODE_ENV = 'production';
+
 function Connect ()
-{
-	
+{	
     var config = {
-		/*
         user: process.env.SQL_USER,
         password: process.env.SQL_PASSWORD,
         database: process.env.SQL_DATABASE
-		*/
-		user: "huhu",
-        password: "password",
-        database: "ecs193_database"
-    
 	};
 
      if (process.env.INSTANCE_CONNECTION_NAME && process.env.NODE_ENV === 'production') 
         config.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
     
-    if (process.env.SQL_HOST && process.env.NODE_ENV != 'production')
-        config.host = process.env.SQL_HOST;
+    if (process.env.NODE_ENV != 'production') {
+        config.host = '35.199.174.8';
+        config.user = 'huhu';
+        config.password = 'password';
+        config.database = 'ecs193_database';
+    }
 
     var knex = Knex({
         client: 'mysql',
@@ -49,99 +46,48 @@ function Connect ()
     return knex;
 }
 
-app.get('/', function (req, res, next)
-{
+app.get('/', function (req, res, next) {
     res.status(404)
         .set('Content-Type', 'text/plain')
-        .send('Please use subdirectories \'/fetch\' or \'/insert\'')
+        .send(' ')
         .end();
 });
 
-app.get('/fetch', function (req, res, next)
-{
- 	knex.select("*")
-    .from('table-test')
-    .orderBy('id')
-    .limit(10)
-    .then((results) => {
-      return results.map((visit) => `id: ${visit.id}, 
-	  ch1: ${visit.ch1}, 
-	  ch2: ${visit.ch2}, 
-	  ch3: ${visit.ch3}, 
-	  ch4: ${visit.ch4}`);
-    }).then((visits) => {
-      res
-        .status(200)
+app.get('/fetch', function (req, res, next) {
+    res.status(200)
         .set('Content-Type', 'text/plain')
-        .send(`Database entries :\n${visits.join('\n')}`)
+        .send('In Development')
         .end();
-    })
-    .catch((err) => {
-      next(err);
-    }); 
-	//SELECT table_name FROM information_schema.tables;
-    //FetchRequestHandler.testFetch(knex, res);
-/* 	knex.select('timestamp', 'userIp')
-    .from('visits')
-    .orderBy('timestamp', 'desc')
-    .limit(10)
-    .then((results) => {
-      return results.map((visit) => `Time: ${visit.timestamp}, AddrHash: ${visit.userIp}`);
-    }).then((visits) => {
-      res
-        .status(200)
-        .set('Content-Type', 'text/plain')
-        .send(`Last 10 visits:\n${visits.join('\n')}`)
-        .end();
-    })
-    .catch((err) => {
-      next(err);
-    }); */
-	//SELECT table_name FROM information_schema.tables;
-    //FetchRequestHandler.testFetch(knex, res);
 });
 
-/**
-*	Intended for single insertion.
-*   Post format should be application/x-www-form-urlencoded:
-*   Body format
-*   i.e.
-		testdb:{parentid: 2, float1: 1, float2:2,float4:2}
-*/
-app.post('/insert', express.urlencoded({ extended: true }),  function (req, res, next)
-{
-	if(!req.is('application/x-www-form-urlencoded'))
-		return next()
-	console.log('Content-Type \'application/x-www-form-urlencoded\'')
-	
-	knex('testtable')
-    InsertRequestHandler.testInsert(knex, req, res);
-}); 
-/**
-*	Intended for large volume insertion.
-*   Post format should be multipart/form-data:
-*   Data should be entered in JSON style. The first key should specify the table name
-	i.e.
-	testdb:{ "parentid": 2, "float1": 1, "float2":2,"float4":2 }
-	testdb:{ "parentid": 3, "float1": 1, "float2":2,"float4":2 }
-	testdb:{ "parentid": 4, "float1": 1, "float2":2,"float4":2 }
-	testdb:{ "parentid": 5, "float1": 1, "float2":2,"float4":2 }
-*/
-app.post('/insert', upload.fields([]), function (req, res, next)
-{
-	if(!req.is('multipart/form-data'))	
-		return next()
-	
-	console.log('Content-Type \'multipart/form-data\'')
-	
-	console.log(typeof(req.body))
-    InsertRequestHandler.testInsert(knex, req, res);
+app.post('/insert/doctor', jsonParser, function (req, res, next) {
+	if(!req.is('application/json'))
+		return next();
+    InsertRequestHandler.insertDoctor(knex, req, res);
 });
-app.post('/insert', function (req, res)
-{
-	res.status(400)
+
+app.post('/insert/patient', jsonParser, function (req, res, next) {
+	if(!req.is('application/json'))
+		return next();
+    InsertRequestHandler.insertPatient(knex, req, res);
+});
+
+app.post('/insert/reading', jsonParser, function (req, res, next) {
+    if(!req.is('application/json'))
+        return next();
+    InsertRequestHandler.insertReading(knex, req, res);
+});
+
+app.post('/insert/reading', upload.fields([]), function (req, res, next) {
+    if(!req.is('multipart/form-data'))
+        return next();
+    InsertRequestHandler.insertReading(knex, req, res);
+});
+
+app.post('/insert/reading', function (req, res, next) {
+    res.status(404)
         .set('Content-Type', 'text/plain')
-        .send('Bad Content-Type')
+        .send('You took a wrong turn somewhere.')
         .end();
 });
 
