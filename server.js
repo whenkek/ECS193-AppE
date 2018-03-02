69793d3189ccba512e2a70d172ceabd5e6dc791a
TyrannosaurusText <pizzalore@gmail.com>
2018-03-01 18:21:40 -0800

    Some comments added.

--- server.js
+++ server.js
@@ -22,25 +22,25 @@ process.env.NODE_ENV = 'production';
 
 var knex = Connect()
 
-function Connect ()
+function Connect () //establish connection with database
 {	
-    var config = {
+    var config = { //make sure your environment variables are set. This is for creating the proxy connection
         user: process.env.SQL_USER,
         password: process.env.SQL_PASSWORD,
         database: process.env.SQL_DATABASE
 	};
 
      if (process.env.INSTANCE_CONNECTION_NAME && process.env.NODE_ENV === 'production') 
-        config.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
+        config.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`; //sets path to databse
     
-    if (process.env.NODE_ENV != 'production') {
-        config.host = '35.199.174.8';
+    if (process.env.NODE_ENV != 'production') { // This is for when the program is deployed onto GoogleApp engine
+        config.host = '35.199.174.8'; 
         config.user = 'huhu';
         config.password = 'password';
         config.database = 'ecs193_database';
     }
 
-    var knex = Knex({
+    var knex = Knex({ //setting knex config properties
         client: 'mysql',
         connection: config
     }); 
@@ -56,35 +56,40 @@ app.get('/', function (req, res, next) {
 });
 
 //FETCHES
-
+/**
+*   This creates a POST request for the list of doctors registered in the database.
+**/
 app.post('/fetch/doctors', function (req, res, next) {
     if (!req.is('application/json'))
         return next();
     FetchRequestHandler.fetchDoctors(knex, req, res);
 });
 
+/**
+*   This creates a POST request for the id that matches the given email specified in the 'email' property of the request header.
+**/
 app.post('/fetch/idFromEmail', function (req, res, next) {
     if (!req.is('application/json'))
         return next();
     FetchRequestHandler.fetchIDfromEmail(knex, req, res);
 });
 
+/**
+*   This creates a POST request for the list of patients that are managed by the doctor specified in the 'doctor' property of the request body.
+**/
 app.post('/fetch/doctorList', function (req, res, next) {
     if (!req.is('application/json'))
         return next();
     FetchRequestHandler.fetchDoctorPatients(knex, req, res);
 });
 
+/**
+*   This creates a POST request for the readings for the id specified in the 'id' property of the request body.
+**/
 app.post('/fetch/readings', function (req, res, next) {
     if (!req.is('application/json'))
         return next();
     FetchRequestHandler.fetchReadings(knex, req, res);
-});
-
-app.post('/fetch/doctorReadings', function (req, res, next) {
-    if (!req.is('application/json'))
-        return next();
-    FetchRequestHandler.fetchDoctorReadings(knex, req, res);
 });
 
 //INSERTS
